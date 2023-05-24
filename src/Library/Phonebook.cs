3500af84d9a8682a36e7bd563e219221b60a3241
apereira <anthony.pereira.utu@gmail.com>
2023-05-24 10:43:21 -0300

    Se cumplen con los requerimientos de la clase phonebook

--- src/Library/Phonebook.cs
+++ src/Library/Phonebook.cs
@@ -12,8 +12,10 @@ namespace Library
             this.persons = new List<Contact>();
         }
 
+        //Conocer el dueño
         public Contact Owner { get; }
 
+    
         public List<Contact> Search(string[] names)
         {
             List<Contact> result = new List<Contact>();
@@ -30,6 +32,30 @@ namespace Library
             }
 
             return result;
+        }
+
+        //Agregar contactos a la lista
+        public void Add(Contact person)
+        {
+            this.persons.Add(person);
+        }
+
+        //Quitar contactos de la lista
+        public void Remove(Contact person)
+        {
+            this.persons.Remove(person);
+        }
+
+        //Enviar un mensaje a contactos dados sus nombres a través de un canal
+        public void Send(string[] names, Message message, IMessageChannel chanel)
+        {
+            List<Contact> contacts = this.Search(names);
+
+            foreach (Contact contact in contacts)
+            {
+                Console.WriteLine($"Sending message to {contact.Name}...");
+                chanel.Send(message, contact);
+            }
         }
     }
 }
